888cb12578420a2c095e0bf26531f8307f820a26
GhassenEljday <ghassenjday2001@gmail.com>
2020-10-02 15:05:08 +0100

    not for pushing

--- quad/src/index.js
+++ quad/src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 // import Post from './components/company/Home/Posts/Post.js'
+import CompanySection from "./components/company/CompanySection"
+
+ReactDOM.render(
+  <React.StrictMode>
+    <CompanySection />-
+  </React.StrictMode>,
+  document.getElementById("root")
+);
 
 // ReactDOM.render(
 //   <React.StrictMode>
